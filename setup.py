5801b4124f31f5225172076f44af13bf560f84ad
LeeW <cl87654321@gmail.com>
2019-04-03 22:08:25 +0800

    fix(setup): Fix setup.py style

--- setup.py
+++ setup.py
@@ -4,26 +4,32 @@
 from distutils.core import setup, Extension
 
 
-setup(name='templater',
-      version='0.4.0',
-      description=('Extract template (a pattern) from strings and parse other'
-                   'strings with this pattern.'),
-      long_description=open('README.rst').read(),
-      author=u'Álvaro Justen',
-      author_email='alvarojusten@gmail.com',
-      url='https://github.com/turicas/templater/',
-      py_modules=['templater'],
-      ext_modules=[Extension('_templater', ['templater.c'])],
-      keywords=['template', 'reversed template', 'template making',
-                'wrapper induction'],
-      classifiers = [
+setup(
+    name='templater',
+    version='0.4.0',
+    description=(
+        'Extract template (a pattern) from strings and parse other'
+        'strings with this pattern.'
+    ),
+    long_description=open('README.rst').read(),
+    author=u'Álvaro Justen',
+    author_email='alvarojusten@gmail.com',
+    url='https://github.com/turicas/templater/',
+    py_modules=['templater'],
+    ext_modules=[
+        Extension('_templater', ['templater.c'])
+    ],
+    keywords=[
+        'template', 'reversed template', 'template making', 'wrapper induction'
+    ],
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
-      ],
+    ],
 )
